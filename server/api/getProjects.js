b92fedcf9c3c384337c26abdecb4caad239c1012
Dana <dana_ciobo@yahoo.com>
2024-04-28 12:41:05 +0100

    updated cors in  BE

--- server/api/getProjects.js
+++ server/api/getProjects.js
@@ -1,4 +1,3 @@
-
 import Project from "../models/project.js";
 import mongoose from 'mongoose';
 import { config } from 'dotenv';
@@ -16,6 +15,12 @@ export default async function handler(req, res) {
 
     // Close the database connection
     await mongoose.disconnect();
+
+    // Set CORS headers
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
 
     // Send the projects as a JSON response
     res.status(200).json(projects);
